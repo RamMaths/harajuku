86c055fde7a221e7a969327e2d7a8d41a7e3a926
Joakiiim <kevin.joaquim.rdz@gmail.com>
2025-04-09 09:18:12 -0600

    Update login http handler (#18)

--- internal/core/port/auth.go
+++ internal/core/port/auth.go
@@ -18,5 +18,5 @@ type TokenService interface {
 // UserService is an interface for interacting with user authentication-related business logic
 type AuthService interface {
 	// Login authenticates a user by email and password and returns a token
-	Login(ctx context.Context, email, password string) (string, error)
+	Login(ctx context.Context, email, password string) (token string, role domain.UserRole, err error)
 }
--- internal/core/service/auth.go
+++ internal/core/service/auth.go
@@ -27,24 +27,24 @@ func NewAuthService(repo port.UserRepository, ts port.TokenService) *AuthService
 }
 
 // Login gives a registered user an access token if the credentials are valid
-func (as *AuthService) Login(ctx context.Context, email, password string) (string, error) {
+func (as *AuthService) Login(ctx context.Context, email, password string) (token string, role domain.UserRole, err error) {
 	user, err := as.repo.GetUserByEmail(ctx, email)
 	if err != nil {
 		if err == domain.ErrDataNotFound {
-			return "", domain.ErrInvalidCredentials
+			return "", "", domain.ErrInvalidCredentials
 		}
-		return "", domain.ErrInternal
+		return "", "", domain.ErrInvalidCredentials
 	}
 
 	err = util.ComparePassword(password, user.Password)
 	if err != nil {
-		return "", domain.ErrInvalidCredentials
+		return "", "", domain.ErrInvalidCredentials
 	}
 
 	accessToken, err := as.ts.CreateToken(user)
 	if err != nil {
-		return "", domain.ErrTokenCreation
+		return "", "", domain.ErrInvalidCredentials
 	}
 
-	return accessToken, nil
+	return accessToken, user.Role, nil
 }
