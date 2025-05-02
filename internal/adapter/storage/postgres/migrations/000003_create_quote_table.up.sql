40655031ddc621c7f26b46ec6637dce8799a5832
Ramses <ramses.hdz30@gmail.com>
2025-05-02 15:00:03 -0600

    Update  quote migration to add quote_state_enum

--- internal/adapter/storage/postgres/migrations/000003_create_quote_table.up.sql
+++ internal/adapter/storage/postgres/migrations/000003_create_quote_table.up.sql
@@ -1,10 +1,12 @@
+CREATE TYPE "quote_state_enum" AS ENUM ('pending', 'approved', 'rejected', 'requires_proof');
+
 CREATE TABLE "Quote" (
 	"id" UUID NOT NULL UNIQUE,
 	"typeOfServiceId" UUID NOT NULL,
 	"clientId" UUID NOT NULL,
 	"time" TIMESTAMPTZ NOT NULL,
 	"description" TEXT,
-	"state" TEXT NOT NULL,
+	"state" quote_state_enum NOT NULL,
 	"price" REAL NOT NULL,
 	"testRequired" BOOLEAN NOT NULL,
     
