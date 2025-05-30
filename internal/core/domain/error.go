d11d897b163d91ee8cab66a47972c62157c3d5b0
Joakiiim <kevin.joaquim.rdz@gmail.com>
2025-05-30 01:49:09 -0600

    Ya no se puede agendar citas si la cotización no esta en [pendiente de pago o requiere prueba de mechón] y también si agendas una cita cuando se requiere prueba de mechón se crea esta automaticamente como agendada.

--- internal/core/domain/error.go
+++ internal/core/domain/error.go
@@ -37,4 +37,6 @@ var (
 	ErrUnauthorized = errors.New("user is unauthorized to access the resource")
 	// ErrForbidden is an error for when the user is forbidden to access the resource
 	ErrForbidden = errors.New("user is forbidden to access the resource")
+	// ErrForbiden cuando no se puede crear una cita porque la cita no se encuentra en pendiente de pago o requiere prueba de mechon
+	ErrForbidenAppointment = errors.New("you cannot create an appointment because the quote is not in pending payment or requires a payment proof")
 )
